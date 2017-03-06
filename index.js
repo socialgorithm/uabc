f130e82c248300ddf01689944b8f3b9acb300848
Alejandro U. Alvarez <aluralma@gmail.com>
2017-03-06 11:06:03 +0000

    add log to file

--- index.js
+++ index.js
@@ -10,58 +10,81 @@
 const options = require('./lib/input')();
 const exec = require('./lib/exec');
 const io = require('socket.io-client');
+const fileLoggerModule = require('./lib/log-file');
+const consoleLogger = require('./lib/log-console');
 
-console.info("+----------------------------------+");
-console.info("|     Ultimate Algorithm Battle    |");
-console.info("+----------------------------------+");
+main();
 
-console.log();
-console.log('Waiting for server...');
-console.log();
+function main() {
+  console.info("+----------------------------------+");
+  console.info("|     Ultimate Algorithm Battle    |");
+  console.info("+----------------------------------+");
 
-function log(writer, data) {
-  if (options.verbose) {
-    const time = (new Date()).toTimeString().substr(0,8);
-    console.log('[' + time + ' ' + writer + '] ' + data);
-  }
-}
-
-// Spawn the player
-try {
-  const player = exec(options.file);
-
-  const socket = io.connect('http://localhost:3141', {
-    query: {
-      token: options.token
+  // Init logger
+  let fileLogger;
+  if (options.log) {
+    if (options.log.length > 0) {
+      fileLogger = fileLoggerModule(options.log);
+    } else {
+      const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').replace(/ /, '_');
+      fileLogger = fileLoggerModule('uabc-' + date + '.log');
     }
-  });
+  }
 
-  player.on('close', (code) => {
-    console.log(`client> child process exited with code ${code}`);
-    socket.disconnect();
-  });
+  console.log();
+  console.log('Waiting for server...');
+  console.log();
 
-  socket.on('error', function (data) {
-    console.error('Error in socket', data);
-  });
+  function log(writer, data) {
+    if (options.verbose) {
+      consoleLogger(writer, data);
+    }
+    if (options.log) {
+      fileLogger(writer, data);
+    }
+  }
 
-  socket.on('connect', function(){
-    console.log('Connected!');
-  });
+  // Spawn the player
+  try {
+    const player = exec(options.file);
+    let host = options.host || 'localhost:3141';
+    if (host.substr(0,4) !== 'http') {
+      host = 'http://' + host;
+    }
 
-  socket.on('game', function (data) {
-    log('server', data.action);
-    player.stdin.write(data.action + "\n");
-  });
+    const socket = io.connect(host, {
+      query: {
+        token: options.token
+      }
+    });
 
-  player.stdout.on('data', function(data) {
-    log('player', data);
-    socket.emit('game', data);
-  });
+    player.on('close', (code) => {
+      console.log(`client> child process exited with code ${code}`);
+      socket.disconnect();
+    });
 
-  socket.on('disconnect', function() {
-    console.log('Connection lost!');
-  });
-} catch (e) {
-  console.error('Error running player', e);
+    socket.on('error', function (data) {
+      console.error('Error in socket', data);
+    });
+
+    socket.on('connect', function(){
+      console.log('Connected!');
+    });
+
+    socket.on('game', function (data) {
+      log('server', data.action);
+      player.stdin.write(data.action + "\n");
+    });
+
+    player.stdout.on('data', function(data) {
+      log('player', data);
+      socket.emit('game', data);
+    });
+
+    socket.on('disconnect', function() {
+      console.log('Connection lost!');
+    });
+  } catch (e) {
+    console.error('Error running player', e);
+  }
 }
