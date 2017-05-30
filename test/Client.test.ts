b7104e4d3964f75ea4afbca0e9b243018dc855c8
Alejandro U. Alvarez <aluralma@gmail.com>
2017-05-30 11:20:06 +0100

    add initial test structure

---
+++ test/Client.test.ts
@@ -0,0 +1,16 @@
+import { expect } from 'chai';
+import PracticeClient from '../src/PracticeClient';
+
+describe('PracticeClient', () => {
+
+    let subject: PracticeClient;
+
+    beforeEach(() => {
+        subject;
+    });
+
+    describe('#playerTokens()', () => {
+
+    });
+
+});
