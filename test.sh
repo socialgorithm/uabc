5666650ebf71c6fbb3ceab7292b8776c1b261c24
Alejandro U. Alvarez <aluralma@gmail.com>
2018-08-02 17:58:17 +0100

    add test script to launch multiple

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+LOBBY=$1
+NUMBER_PLAYERS=$2
+HOST=http://localhost:3141
+
+for i in `seq 1 $NUMBER_PLAYERS`;
+do
+    yarn start --host $HOST --lobby $LOBBY --token "Player $i" -f "node dist/sample/player.js" > multitest.log  &
+done
+
+echo "All clientes started"
+echo
+
+read -p "Press enter to stop all uabc processes..."
+
+ps -e | grep $LOBBY | awk '{print $1}' | xargs kill -9
